f5d31f7953dabc2cba7393dac9f97f82bd5c7097
TaeKwang-Ko <rhxorhkd@naver.com>
2016-10-15 23:04:50 +0900

    Android -> 탭, home 페이지 구현

---
+++ Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.rhxorhkd.android_seoulyeojido;
+
+import android.app.ActivityGroup;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends ActivityGroup {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent;
+        TabHost tabHost = (TabHost)findViewById(R.id.tabhost);
+        tabHost.setup(getLocalActivityManager()); //intent를 content로 하기 위해서 getlocalActivitymanager 필요
+        TabHost.TabSpec spec;
+        //setindicator -> 탭에 지시자로, 라벨과 아이콘 지정, setIndicator(CharSequence label, Drawable icon)
+        //Setcontent -> 탭의 내용물 XML 문서의 뷰 이용, setContent(Intent intent) -> 인텐트를 이용하여 다른 액티비티 호출
+
+        intent = new Intent(this,home.class);
+        spec = tabHost.newTabSpec("Tab1").setContent(intent).setIndicator("home");
+        tabHost.addTab(spec);
+
+        intent = new Intent(this,checkin.class);
+        spec = tabHost.newTabSpec("Tab2").setContent(intent).setIndicator("checkin");
+        tabHost.addTab(spec);
+
+        intent = new Intent(this,rank.class);
+        spec = tabHost.newTabSpec("Tab3").setContent(intent).setIndicator("rank");
+        tabHost.addTab(spec);
+
+        intent = new Intent(this,set.class);
+        spec = tabHost.newTabSpec("Tab4").setContent(intent).setIndicator("set");
+        tabHost.addTab(spec);
+
+
+        //높이설정
+        //tabHost.getTabWidget().getChildAt(0).getLayoutParams().height=80;
+
+        tabHost.setCurrentTab(0); //첫 시작
+    }
+}
---
+++ Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/home.java
@@ -0,0 +1,145 @@
+package com.example.rhxorhkd.android_seoulyeojido;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.util.ArrayList;
+
+public class home extends AppCompatActivity {
+    private static final String TAG = "DemoActivity";
+    private SlidingUpPanelLayout mLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button button =(Button) findViewById(R.id.button);
+        ListView lv =(ListView) findViewById(R.id.list);
+        ImageView imageView6 =(ImageView)findViewById(R.id.imageView6);
+
+        ArrayList<Listviewitem> data = new ArrayList<>();
+        Listviewitem data1 = new Listviewitem(R.drawable.ic_launcher,"one");
+        Listviewitem data2 = new Listviewitem(R.drawable.ic_launcher,"two");
+        Listviewitem data3 = new Listviewitem(R.drawable.ic_launcher,"three");
+        Listviewitem data4 = new Listviewitem(R.drawable.ic_launcher,"four");
+        Listviewitem data5 = new Listviewitem(R.drawable.ic_launcher,"five");
+        Listviewitem data6 = new Listviewitem(R.drawable.ic_launcher,"six");
+        Listviewitem data7 = new Listviewitem(R.drawable.ic_launcher,"seven");
+        Listviewitem data8 = new Listviewitem(R.drawable.ic_launcher,"eight");
+        Listviewitem data9 = new Listviewitem(R.drawable.ic_launcher,"nine");
+        Listviewitem data10 = new Listviewitem(R.drawable.ic_launcher,"tem");
+        Listviewitem data11 = new Listviewitem(R.drawable.ic_launcher,"eleven");
+
+        data.add(data1);
+        data.add(data2);
+        data.add(data3);
+        data.add(data4);
+        data.add(data5);
+        data.add(data6);
+        data.add(data7);
+        data.add(data8);
+        data.add(data9);
+        data.add(data10);
+        data.add(data11);
+
+        final ListviewAdapter adapter = new ListviewAdapter(this,R.layout.item,data);
+        lv.setAdapter(adapter);
+
+        mLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG,"onPanelSlide, offset "+slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i(TAG,"onPanelStateChanged" + newState);
+            }
+        });
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = (String)adapter.getItem(position);
+                //Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG).show(); //옮기고 이거써
+                Toast.makeText(home.this,str,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mLayout != null &&
+                (mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED || mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.ANCHORED)) { //펼쳐있을때 back 누르면 닫히는거
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //search
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_view,menu);
+        MenuItem mSearch = menu.findItem(R.id.search);
+
+
+        final SearchView searchView  = (SearchView) MenuItemCompat.getActionView(mSearch);
+        searchView.setIconified(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Log.d("text",query); -> query 찍힘
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
