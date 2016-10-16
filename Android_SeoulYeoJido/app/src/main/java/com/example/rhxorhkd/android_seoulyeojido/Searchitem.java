66a8b6dd28498322c2ac193936a393fb87f75fe1
TaeKwang-Ko <rhxorhkd@naver.com>
2016-10-17 02:24:07 +0900

    search -> framelayout 화면 이동,  text filter 구현

---
+++ Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/Searchitem.java
@@ -0,0 +1,20 @@
+package com.example.rhxorhkd.android_seoulyeojido;
+
+/**
+ * Created by rhxorhkd on 2016-10-17.
+ */
+
+public class Searchitem {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Searchitem(String name){
+        this.name=name;
+    }
+}
--- Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/home.java
+++ Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/home.java
@@ -1,8 +1,10 @@
 package com.example.rhxorhkd.android_seoulyeojido;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,6 +23,11 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import java.util.ArrayList;
 
 public class home extends AppCompatActivity {
+    SearchitemAdapter sadapter; //search adapter
+    LinearLayout map; //지도
+    LinearLayout searchlistview; //검색창누르면 나오는 리스트뷰
+    Button button; // 지도 안의 버튼
+    SearchView searchView; //서치뷰
     private static final String TAG = "DemoActivity";
     private SlidingUpPanelLayout mLayout;
     @Override
@@ -27,7 +35,54 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button button =(Button) findViewById(R.id.button);
+
+        ListView listView1 = (ListView)findViewById(R.id.listview1);
+        ArrayList<Searchitem> searchdatas = new ArrayList<>();
+        Searchitem searchdata1 = new Searchitem("dog");
+        Searchitem searchdata2 = new Searchitem("eagle");
+        Searchitem searchdata3 = new Searchitem("pig");
+        Searchitem searchdata4 = new Searchitem("tiger");
+        Searchitem searchdata5 = new Searchitem("lion");
+        Searchitem searchdata6 = new Searchitem("bird");
+        Searchitem searchdata7 = new Searchitem("human");
+        Searchitem searchdata8 = new Searchitem("ant");
+        Searchitem searchdata9 = new Searchitem("kotaekwang");
+        Searchitem searchdata10 = new Searchitem("baek");
+        Searchitem searchdata11 = new Searchitem("seo");
+        Searchitem searchdata12 = new Searchitem("han");
+
+        searchdatas.add(searchdata1);
+        searchdatas.add(searchdata2);
+        searchdatas.add(searchdata3);
+        searchdatas.add(searchdata4);
+        searchdatas.add(searchdata5);
+        searchdatas.add(searchdata6);
+        searchdatas.add(searchdata7);
+        searchdatas.add(searchdata8);
+        searchdatas.add(searchdata9);
+        searchdatas.add(searchdata10);
+        searchdatas.add(searchdata11);
+        searchdatas.add(searchdata12);
+
+        sadapter = new SearchitemAdapter(this,R.layout.searchitem,searchdatas);
+        listView1.setAdapter(sadapter);
+
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("text","listview1clicked");
+                String str = (String)sadapter.getItem(position);
+                //Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG).show(); //옮기고 이거써
+                Toast.makeText(home.this,str,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+        map=(LinearLayout)findViewById(R.id.map);
+        searchlistview=(LinearLayout)findViewById(R.id.searchlistview);
+        button =(Button) findViewById(R.id.button);
+
         ListView lv =(ListView) findViewById(R.id.list);
         ImageView imageView6 =(ImageView)findViewById(R.id.imageView6);
 
@@ -89,7 +144,6 @@ public class home extends AppCompatActivity {
             }
         });
 
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -104,7 +158,6 @@ public class home extends AppCompatActivity {
             }
         });
 
-
     }
 
     @Override
@@ -117,16 +170,37 @@ public class home extends AppCompatActivity {
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) { //search
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.action_view,menu);
         MenuItem mSearch = menu.findItem(R.id.search);
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        MenuItemCompat.setOnActionExpandListener(mSearch, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {  //서치 아이콘 누를때  expand
+                Log.d("text","open");
+                map.setVisibility(View.INVISIBLE); // map화면 가려짐
+                searchlistview.setVisibility(View.VISIBLE); //searchlist 화면 켜짐
+                return true;
+            }
 
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) { //서치 끌때
+                Log.d("text","close");
+                searchlistview.setVisibility(View.INVISIBLE);
+                map.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
 
-        final SearchView searchView  = (SearchView) MenuItemCompat.getActionView(mSearch);
+        searchView  = (SearchView) MenuItemCompat.getActionView(mSearch);
+
+        searchView.setSubmitButtonEnabled(true);
         searchView.setIconified(true);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
             @Override
             public boolean onQueryTextSubmit(String query) {
                 // Log.d("text",query); -> query 찍힘
@@ -137,8 +211,11 @@ public class home extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                sadapter.filter(newText);
+                Log.d("text",""+newText);
                 return false;
             }
+
         });
         return super.onCreateOptionsMenu(menu);
     }
