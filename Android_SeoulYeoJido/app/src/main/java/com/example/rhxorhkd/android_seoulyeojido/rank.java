976266269dd717fe371227a4517d38a2950a3d7c
lrcr <handsomeby@naver.com>
2016-10-28 00:47:08 +0900

    나의 커밋가랏@

--- Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/rank.java
+++ Android_SeoulYeoJido/app/src/main/java/com/example/rhxorhkd/android_seoulyeojido/rank.java
@@ -1,5 +1,6 @@
 package com.example.rhxorhkd.android_seoulyeojido;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +12,13 @@ import android.widget.Toast;
 
 import com.example.rhxorhkd.android_seoulyeojido.Model.RankItem;
 import com.example.rhxorhkd.android_seoulyeojido.RankRecyclerView.RankAdapter;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,9 +26,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class rank extends AppCompatActivity implements View.OnClickListener {
 
+    static int my ;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -30,6 +39,11 @@ public class rank extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth auth;
     private FirebaseDatabase db;
     private DatabaseReference ref;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,56 +56,107 @@ public class rank extends AppCompatActivity implements View.OnClickListener {
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
         ref = db.getReference("member");
-        FirebaseUser user = auth.getCurrentUser();
-
+        final FirebaseUser user = auth.getCurrentUser();
 
         findViewById(R.id.find_myRank).setOnClickListener(this);
 
         final ArrayList<RankItem> list = new ArrayList<>();
-        mRecyclerView = (RecyclerView) findViewById(R.id.rank_rc);
+        final Comparator<RankItem> sort = new Comparator<RankItem>() {
+            public int compare(RankItem r1, RankItem r2) {
+                return r1.getChk_cnt().compareTo(r2.getChk_cnt())>0 ? -1: 1;
+            }
+        };
+
+
 
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot data) {
-                int cnt = 0;
                 for (DataSnapshot post: data.getChildren()) {
+                    int cnt = list.size();
 
-                    String nick = ""+post.child("nickname").getValue();
-                    String chk_cnt = ""+post.child("checkin").getChildrenCount();
-                    String img = ""+post.child("profile").getValue();
+                    if(post.getKey().toString().equals(user.getUid().toString())){
+                        my = cnt+1;
+                    }
+
+                    String nick = "" + post.child("nickname").getValue();
+                    String chk_cnt = "" + post.child("checkin").getChildrenCount();
+                    String img = "" + post.child("profile").getValue();
 
                     RankItem item = new RankItem(
-                           ""+(cnt+1), nick , chk_cnt , "", img
+                            "" + cnt , nick, chk_cnt, "", img
                     );
                     list.add(cnt, item);
-                    cnt++;
+                    Collections.sort(list, sort);
 
-                    mAdapter = new RankAdapter(rank.this, list);
-                    mLayoutManager = new LinearLayoutManager(rank.this, LinearLayoutManager.VERTICAL, false);
-                    mRecyclerView.setLayoutManager(mLayoutManager);
-                    mRecyclerView.setAdapter(mAdapter);
+
+                    if(cnt+1 == data.getChildrenCount()){
+                        mRecyclerView = (RecyclerView) findViewById(R.id.rank_rc);
+                        mLayoutManager = new LinearLayoutManager(rank.this, LinearLayoutManager.VERTICAL, false);
+                        mRecyclerView.setLayoutManager(mLayoutManager);
+                        mAdapter = new RankAdapter(rank.this, list);
+                        mRecyclerView.setAdapter(mAdapter);
+                    }
+
                 }
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
 
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.find_myRank:
-//                    mLayoutManager.scrollToPosition(95);
-                mLayoutManager.smoothScrollToPosition(mRecyclerView, null, 95);
+                    mLayoutManager.scrollToPosition(my);
+//                mLayoutManager.smoothScrollToPosition(mRecyclerView, null, 95);
                 break;
             default:
                 break;
         }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("rank Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
